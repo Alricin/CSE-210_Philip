7f120aef2f932edaccbcfba61e2582460894d849
alricin <cmwals42@gmail.com>
2023-01-09 11:46:41 -0700

    The second prep assignment's core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage");
+        string Score = Console.ReadLine();
+
+        int Grade = int.Parse(Score);
+        string Verdict = "N/A";
+
+        if (Grade >= 90)
+        {
+            Verdict = "A";
+        }
+        else if (Grade >= 80)
+        {
+            Verdict = "B";
+        }
+        else if (Grade >= 70)
+        {
+            Verdict = "C";
+        }
+        else if (Grade >= 60)
+        {
+            Verdict = "D";
+        }
+        else 
+        {
+            Verdict = "F";
+        }
+        Console.WriteLine($"Your grade is {Verdict}");
+        
+        if (Grade >= 70)
+        {
+            Console.WriteLine("Congratulations!! You passed");
+        }
+        else 
+        {
+            Console.WriteLine("You failed. Good luck next time!");
+        }
+
     }
 }
