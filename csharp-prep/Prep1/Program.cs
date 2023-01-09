35298e4c6e9c9280bc5e2f8020b76f574a793bbc
alricin <cmwals42@gmail.com>
2023-01-09 11:07:05 -0700

    An edit for visual appeal

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,8 +6,10 @@ class Program
     {
         Console.WriteLine("What is your first name");
         string First = Console.ReadLine();
+
         Console.WriteLine("What is your last name");
         string Last = Console.ReadLine();
+        
         Console.WriteLine($"Your name is {Last}. {First} {Last}.");
     }
 }
