fbd51451df9af519f5ef503fb4914472ba9c85c6
alricin <cmwals42@gmail.com>
2023-01-25 10:37:34 -0700

    Prep 3, requirements 1,2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,6 +7,29 @@ class Program
         Console.WriteLine("What is your number?");
         string chosen_number = Console.ReadLine();
         int goal_number = int.Parse(chosen_number);
+        bool guess_status = false;
+        while (guess_status == false)
+        {
+            Console.WriteLine("What is your guess?");
+            string guess_number = Console.ReadLine();
+            int comparative_number = int.Parse(guess_number); 
+            if (comparative_number == goal_number)
+            {
+                guess_status = true;
+                Console.WriteLine("That's correct");
+            }
+            else if (comparative_number > goal_number)
+            {
+                guess_status = false;
+                Console.WriteLine("Guess Lower");
+            }
+            else if (comparative_number < goal_number)
+            {
+                guess_status = false;
+                Console.WriteLine("Guess Higher");
+            }
+        }
+        
         
     }
 }
