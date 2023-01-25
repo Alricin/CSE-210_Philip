445c47bb21f34daee8c5513250e5c795cb495b87
alricin <cmwals42@gmail.com>
2023-01-25 10:56:39 -0700

    Compiling the List

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter your list of numbers. Enter 0 when finished");
+        int number_entry = 10;
+        List<int> number_list = new List<int>();
+        while (number_entry != 0)
+        {
+            Console.WriteLine("Enter your number:");
+            string number_input = Console.ReadLine();
+            number_entry = int.Parse(number_input);
+            number_list.Add(number_entry);
+        }
+        
     }
 }
