27cb124a2fd18a0bb13c9e4a051fdd24029c0317
alricin <cmwals42@gmail.com>
2023-01-25 10:41:27 -0700

    Introduced the random number generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,26 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your number?");
-        string chosen_number = Console.ReadLine();
-        int goal_number = int.Parse(chosen_number);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
         bool guess_status = false;
         while (guess_status == false)
         {
             Console.WriteLine("What is your guess?");
             string guess_number = Console.ReadLine();
             int comparative_number = int.Parse(guess_number); 
-            if (comparative_number == goal_number)
+            if (comparative_number == number)
             {
                 guess_status = true;
                 Console.WriteLine("That's correct");
             }
-            else if (comparative_number > goal_number)
+            else if (comparative_number > number)
             {
                 guess_status = false;
                 Console.WriteLine("Guess Lower");
             }
-            else if (comparative_number < goal_number)
+            else if (comparative_number < number)
             {
                 guess_status = false;
                 Console.WriteLine("Guess Higher");
