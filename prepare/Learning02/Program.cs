3b99e4eb24c32482a9f67e889732512d86ec5d7b
alricin <cmwals42@gmail.com>
2023-01-30 10:49:40 -0700

    testing resume class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,6 +9,16 @@ class Program
         job1._JobTitle = "custodial";
         job1._StartYear = "2022";
         job1._EndYear = "2023";
-        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._Company = "Wies Markets";
+        job2._JobTitle = "Deli Associate";
+        job2._StartYear = "2018";
+        job2._EndYear = "2020";
+
+        Resume resume1 = new Resume();
+        resume1._EmployeeName = "Philip";
+        resume1._job.Add(job1);
+        resume1._job.Add(job2);
     }
 }
