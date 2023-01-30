a6bf357d88d770272a2510a73597abba808ed1b0
alricin <cmwals42@gmail.com>
2023-01-30 10:13:26 -0700

    added responsibilities

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,5 +2,9 @@ using System;
 
 public class job
 {
+    public string _Company = "";
+    public string _JobTitle = "";
+    public string _StartYear = "";
+    public string _EndYear = "";
     
 }
