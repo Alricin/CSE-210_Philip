b592f559e04f6b8914ba992560b0cdd34fe125c4
alricin <cmwals42@gmail.com>
2023-02-06 12:19:15 -0700

    Add standard prompt questions

--- prove/Develop02/Write.cs
+++ prove/Develop02/Write.cs
@@ -2,5 +2,16 @@ using System;
 
 public class Write
 {
-    List<string> JournalPrompt = new List<string>();
+    static void Main(string[] args)
+    {
+        List<string> JournalPrompt = new List<string>();
+
+        JournalPrompt.Add("Who was the most important person you met today?");
+        JournalPrompt.Add("What was the best part of my day?");
+        JournalPrompt.Add("How did I see the hand of the Lord in my life today?");
+        JournalPrompt.Add("What was the strongest emotion I felt today?");
+        JournalPrompt.Add("If I had one thing I could do over today, what would it be?");
+
+    }
+
 }
