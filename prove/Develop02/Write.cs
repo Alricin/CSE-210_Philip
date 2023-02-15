f8013d3f699afae3ca8f80c4e3af940141a9835e
alricin <cmwals42@gmail.com>
2023-02-15 12:13:41 -0700

    Completes the Write Class

--- prove/Develop02/Write.cs
+++ prove/Develop02/Write.cs
@@ -2,10 +2,17 @@ using System;
 
 public class Write
 {
-    static void Main(string[] args)
-    {
-        List<string> JournalPrompt = new List<string>();
+    List<string> JournalPrompt = new List<string>();
+    string PromptQuestion;
 
+    public Write()
+    {
+        AddPrompts();
+        ChoosePrompt();
+        DisplayPrompt();
+    }
+    public void AddPrompts()
+    {
         JournalPrompt.Add("Who was the most important person you met today?");
         JournalPrompt.Add("What was the best part of my day?");
         JournalPrompt.Add("How did I see the hand of the Lord in my life today?");
@@ -16,7 +23,21 @@ public class Write
         JournalPrompt.Add("What did I read in the scriptures today?");
         JournalPrompt.Add("What did you eat today that you enjoyed?");
         JournalPrompt.Add("What was a funny joke you thought of or heard today?");
-
+    }
+    public void ChoosePrompt()
+    {
+        Random randomGenerator = new Random();
+        int PromptNumber = randomGenerator.Next(0, 9);
+        PromptQuestion = JournalPrompt[PromptNumber];
+    }
+    public void DisplayPrompt()
+    {
+        Console.WriteLine(PromptQuestion);
     }
 
+    public string WriteEntry()
+    {
+        string JournalEntry = Console.ReadLine();
+        return JournalEntry;
+    }
 }
