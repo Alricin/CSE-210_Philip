59c369c28a534724d8e8cc2ceb21ca95df20782c
alricin <cmwals42@gmail.com>
2023-02-21 12:14:16 -0700

    Revised the Read Class

---
+++ prove/Develop02/Read.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Read
+{
+    string JournalText;
+
+    public Read()
+    {
+        
+    }
+
+    public void DisplayActiveJournal()
+    {
+        Console.WriteLine(JournalText);
+    }
+}
