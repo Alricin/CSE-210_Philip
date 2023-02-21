64d976e759d2f951c2a244c2a0b657e9403b99e3
alricin <cmwals42@gmail.com>
2023-02-21 11:51:52 -0700

    Clearing the main function for remaking

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Menu Selection = new Menu();
-        bool SelectionCycle = true;
-        while (SelectionCycle == true);
-        {
-            Selection.SelectionScreen();
-            
-        }
+       
     }
 }
