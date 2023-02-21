4364b5ff7c4f41eb8111c3493ec5309624cd3022
alricin <cmwals42@gmail.com>
2023-02-21 12:12:38 -0700

    Program written (invokes our classes) While loop for Menu Class If-else for Load, Save, Write, Read

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-       
+        bool MenuActive = true;
+        Menu UserMenu = new Menu();
+        while (MenuActive == true)
+        {
+            UserMenu.ChooseJournalAction();
+            if (UserMenu._SelectionValue == 1)
+            {
+                Write TemporaryJournal = new Write();
+                string TemporaryJournalEntry = TemporaryJournal.WriteEntry();
+            } 
+            else if (UserMenu._SelectionValue == 2)
+            {
+                Read ShownJournalEntry = new Read();
+                ShownJournalEntry.DisplayActiveJournal();
+            }
+            else if (UserMenu._SelectionValue == 3)
+            {
+                Load PermanentJournal = new Load();
+                PermanentJournal.ChooseTextFile();
+            }
+            else if (UserMenu._SelectionValue == 4)
+            {
+                Save PromisingJournal = new Save();
+                PromisingJournal.SaveEntryInJournal();
+            }
+            else
+            {
+                MenuActive = false;
+            }
+        }
     }
 }
