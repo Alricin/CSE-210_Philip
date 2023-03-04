d904c94f392977fe2dc37c80ce21b9c027a68078
alricin <cmwals42@gmail.com>
2023-03-04 12:22:35 -0700

    Main Program Written

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        Fraction Value1 = new Fraction();
+        Value1.PrintDecimalAndFraction();
+
+        Fraction Value2 = new Fraction(5);
+        Value2.PrintDecimalAndFraction();
+
+        Fraction Value3 = new Fraction(3,4);
+        Value3.PrintDecimalAndFraction();
+
+        Fraction Value4 = new Fraction(1,3);
+        Value4.PrintDecimalAndFraction();
         
     }
 }
