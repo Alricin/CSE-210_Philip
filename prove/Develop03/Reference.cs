fde85d8a060565c15ed98244ef1533ac4b6b4c36
alricin <cmwals42@gmail.com>
2023-03-20 08:56:28 -0600

    reference class completed

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,10 +2,23 @@ using System;
 
 public class Reference
 {
-    private int _BookName;
+    private int _reference;
 
-    private int _ChapterNumber;
+    public Reference()
+    {
 
-    private int _VerseNumber;
-    
+    }
+
+    public int ChooseScripture()
+    {
+        Console.WriteLine("Choose one of the following scriptures to memorize:");
+        Console.WriteLine("John 3:16");
+        Console.WriteLine("Proverbs 3:5-6");
+        Console.WriteLine("1 Nephi 3:7");
+        Console.WriteLine('D&C 19:18-19');
+        _reference = Console.ReadLine();
+        return _reference;
+
+    }
+
 }
