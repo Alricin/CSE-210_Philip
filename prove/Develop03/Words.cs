d2eff1ee1d1163a4103271a1c195eeac00a7758b
alricin <cmwals42@gmail.com>
2023-03-21 12:57:23 -0600

    Now tracks the words

--- prove/Develop03/Words.cs
+++ prove/Develop03/Words.cs
@@ -4,17 +4,22 @@ public class Words
 {
     private string _ScriptureText;
     private string _HiddenWord;
+    private string[] word;
+    private int _ScriptureLength;
+    private List<bool> WordVisibility = new List<bool>();
 
     public Words(string ScriptureText)
     {
         _ScriptureText = ScriptureText;
+        SplitTextToWords();
     }
 
     public void SplitTextToWords()
     {
         string[] word = _ScriptureText.Split(" ");
+        _ScriptureLength = word.Length;
     }
-    
+
 
     public void DisplayCurrentText()
     {
