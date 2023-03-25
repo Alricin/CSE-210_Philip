e0ac73948446e13c2ab71f914b7853513c08e976
alricin <cmwals42@gmail.com>
2023-03-25 16:32:56 -0600

    Added third test

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -9,7 +9,7 @@ class Program
         Assignment Test1 = new Assignment(Name, Topic1);
         string Test1Summary = Test1.GetSummary();
         Console.WriteLine(Test1Summary);
-        
+
         string Topic2 = "Fractions";
         string Topic2Section = "7-2";
         string Topic2Problems = "1,3,5,9,15";
@@ -19,5 +19,12 @@ class Program
         Console.WriteLine(Test2Summary);
         Console.WriteLine(Test2Homework);
 
+        string Topic3 = "Creative Writing";
+        string EssayTitle = "The Purple Seas";
+        WritingAssignment Test3 = new WritingAssignment(Name, Topic3, EssayTitle);
+        string Test3Summary = Test3.GetSummary();
+        string Test3Essay = Test3.GetWritingInformation();
+        Console.WriteLine(Test3Summary);
+        Console.WriteLine(Test3Essay);
     }
 }
