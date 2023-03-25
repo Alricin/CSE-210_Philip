4116d3eefaeae63811651b02147398ed91a74f0e
alricin <cmwals42@gmail.com>
2023-03-25 16:17:17 -0600

    Write up of whole WritingAssignment class

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _Title;
+
+    public WritingAssignment(string StudentName, string Topic, string Title) : base(StudentName, Topic)
+    {
+        _Title = Title;
+    }
+    
+    public string GetWritingInformation()
+    {
+        return $"{_Title}";
+    }
+}
