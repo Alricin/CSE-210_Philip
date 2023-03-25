1e929bde15679bd53974cb343429cb217578c92e
alricin <cmwals42@gmail.com>
2023-03-25 16:27:46 -0600

    Added second test information

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -9,6 +9,15 @@ class Program
         Assignment Test1 = new Assignment(Name, Topic1);
         string Test1Summary = Test1.GetSummary();
         Console.WriteLine(Test1Summary);
+        
+        string Topic2 = "Fractions";
+        string Topic2Section = "7-2";
+        string Topic2Problems = "1,3,5,9,15";
+        MathAssignment Test2 = new MathAssignment(Name, Topic2, Topic2Section, Topic2Problems);
+        string Test2Summary = Test2.GetSummary();
+        string Test2Homework = Test2.GetHomeworkList();
+        Console.WriteLine(Test2Summary);
+        Console.WriteLine(Test2Homework);
 
     }
 }
