e43cbea5909ddd1dd616624304a7f9f9665268ba
alricin <cmwals42@gmail.com>
2023-03-25 15:20:51 -0600

    Added Setter

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -4,6 +4,11 @@ class Assignment
 {
     private string _StudentName;
     private string _Topic;
+    public Assignment(string StudentName, string Topic)
+    {
+        _StudentName = StudentName;
+        _Topic = Topic;
+    }
     public string GetSummary()
     {
         return $"{_StudentName} - {_Topic}";
