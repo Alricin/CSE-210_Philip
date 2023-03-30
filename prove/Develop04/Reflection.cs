b82f3636ce5be319fdd9a0b1a0ef06fefdbb6862
alricin <cmwals42@gmail.com>
2023-03-30 13:40:17 -0600

    first half of class written

---
+++ prove/Develop04/Reflection.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Reflection: Activity
+{
+    string _prompt;
+    string questions;
+    int Duration;
+    
+    public Reflection(int Duration) : base(Duration)
+    {
+
+    }
+
+    public void ChoosePrompt();
+    {
+        Random rnd = new Random();
+        
+
+    }
+}
