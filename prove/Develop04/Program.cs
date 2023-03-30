5e6700bf001829fd9b8f8a2bd0c7821851f9a7ed
alricin <cmwals42@gmail.com>
2023-03-30 15:20:48 -0600

    Writing out the formal program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool Quit = false;
+        while (Quit == false)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("   1. Breathing Activity");
+            Console.WriteLine("   2. Reflection Activity");
+            Console.WriteLine("   3. Lisitng Activity");
+            Console.WriteLine("   4. Quit");
+            Console.WriteLine("Select a choice form the Menu");
+            int Choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("How Long would you like to spend on this?");
+            int TimeSpent = int.Parse(Console.ReadLine());
+
+            if (Choice == 1)
+            {
+                Breathing Breaths = new Breathing(TimeSpent);
+                Breaths.DisplayStartingMessage();
+                Breaths.DisplayBreathing(TimeSpent);
+                Breaths.DisplayConcludingMessage();
+            }
+            else if (Choice == 2)
+            {
+                Reflection reflect = new Reflection(TimeSpent);
+                reflect.DisplayStartingMessage();
+                reflect.DisplayPrompt();
+                reflect.DisplayQuestion(TimeSpent);
+                reflect.DisplayConcludingMessage();
+            }
+            else if (Choice == 3)
+            {
+                Listing inspire = new Listing(TimeSpent);
+                inspire.DisplayStartingMessage();
+                inspire.DisplayPrompt();
+                inspire.EncourageUser(TimeSpent);
+                inspire.DisplayConcludingMessage();
+            }
+        }
     }
 }
