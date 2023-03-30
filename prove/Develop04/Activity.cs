262cf5cc022687ea9376c41c6144e3ba7fab25b3
alricin <cmwals42@gmail.com>
2023-03-30 12:05:38 -0600

    Created Activity parent class Wrote variables for class Wrote methods for class

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Activity
+{
+    protected int _Duration;
+    private string Duration;
+    protected string _ActivityType;
+
+    public Activity()
+    {
+
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Welcome to the {_ActivityType} activity.");
+        Console.WriteLine("How long will you be spending on this today?");
+        Duration = Console.ReadLine();
+        _Duration = int.Parse(Duration);
+    }
+
+    public void DisplayConcludingMessage()
+    {
+        Console.WriteLine("Thank you for your time today.");
+        Console.WriteLine("We hope you enjoy until next time.");
+    }
+}
