3952bbf6b4d3a422c8b54d42b8b0859bc1367e47
alricin <cmwals42@gmail.com>
2023-04-03 09:03:20 -0600

    creating the shape list

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,5 +6,14 @@ class Program
     {
         Square Object1 = new Square("red", 7);
         Console.WriteLine(Object1.GetArea());
+
+        List<Shape> Items = new List<Shape>();
+        Items.Add(Object1);
+        Rectangle Object2 = new Rectangle("blue", 4, 5);
+        Circle Object3 = new Circle("yellow", 5);
+        Items.Add(Object2);
+        Items.Add(Object3);
+
+        
     }
 }
