04a5165fac7118a6b0138ec021a25070ff92d9d9
alricin <cmwals42@gmail.com>
2023-04-03 08:49:49 -0600

    Testing the square class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square Object1 = new Square("red", 7);
+        Console.WriteLine(Object1.GetArea());
     }
 }
