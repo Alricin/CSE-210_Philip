dab046d9cdb7dfe8b44d4e14352d1d5f20c88e92
alricin <cmwals42@gmail.com>
2023-04-05 19:44:27 -0600

    Created Artisan class

---
+++ final/FinalProject/ArtisanJobs.cs
@@ -0,0 +1,25 @@
+using System;
+
+class ArtisanJobs:TownJobs
+{
+    public ArtisanJobs()
+    {
+
+    }
+    public override void AddJobs()
+    {
+        Occupations.Add("Actor");
+        Occupations.Add("Alchemist");
+        Occupations.Add("Blacksmith");
+        Occupations.Add("Bowyer");
+        Occupations.Add("Brewer");
+        Occupations.Add("Carpenter");
+        Occupations.Add("Cobbler");
+        Occupations.Add("Dancer");
+        Occupations.Add("Musician");
+        Occupations.Add("Painter");
+        Occupations.Add("Poet");
+        Occupations.Add("Stonemason");
+
+    }
+}
